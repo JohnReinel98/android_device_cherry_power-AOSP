192f357ce39012e5d2a05090aa8558ece9e948fa
ProtoHype98 <johnrenielcaluag@yahoo.com>
2016-08-08 20:05:09 +0800

    Update and rename cm.mk to omni.mk

--- omni.mk
+++ omni.mk
@@ -1,8 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := Cherry X300
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OMNI stuff.
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 
 $(call inherit-product, device/cherry/x300/device.mk)
 
@@ -12,7 +12,7 @@ TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x300
-PRODUCT_NAME := cm_x300
+PRODUCT_NAME := omni_x300
 PRODUCT_BRAND := Cherry
 PRODUCT_MODEL := Flare S3 Power
 PRODUCT_MANUFACTURER := Cherry
